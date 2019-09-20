192b7c4161081d40cee91f1d7310cd34630f6ff7
seboll13 <seboll13@gmail.com>
2019-09-20 12:20:50 +0200

    new function added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
+import static java.lang.System.out;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        out.println("Hello, World !");
+        out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
+    }
+
+    public static int computeUltimateAnswer() {
+        return 6 * 7;
     }
 }
