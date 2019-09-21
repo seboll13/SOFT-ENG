48b9a86cd7b1468e89726c0af2169d21aec16f57
seboll13 <seboll13@gmail.com>
2019-09-21 12:44:33 +0200

    try to push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
+    }
+
+    public static String computeUltimateQuestion() {
+        return "still searching...";
     }
 }
